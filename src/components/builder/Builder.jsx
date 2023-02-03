bb6baff769fcf2b084cda6c0833e38844f46dbc2
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-02-02 22:14:59 -0300

    responsive mode

--- src/components/builder/Builder.jsx
+++ src/components/builder/Builder.jsx
@@ -87,102 +87,142 @@ function Builder() {
 
     const finishedToggle = (e, id) => {
         const newExercises = [...exercises];
-        const exerciseToEdit = newExercises.find((ex) => ex.id === id);
+        const exerciseToEdit = newExercises.find((ex) => ex.id === id)
         exerciseToEdit.finished = e.target.checked;
 
-        localStorage.setItem('exercises', JSON.stringify(newExercises));
+        localStorage.setItem('exercises', JSON.stringify(newExercises))
         setExercises(newExercises);
     }
 
     const [listName, setListName] = useState('')
+    const [listArray, setListArray] = useState(() => {
+        const storedListArray = localStorage.getItem("listArray");
+        return storedListArray ? JSON.parse(storedListArray) : [];
+      });
 
-    const [listArray, setListArray] = useState([])
+    useEffect(() => console.log('after first render'), [listArray.length]);
 
     const handleSave = (e) => {
         e.preventDefault()
 
-        let newList = {
-            name: listName,
-            exercises: exercises,
-            id: Date.now(),
+        if (listName.length>0 && listName.trim('') !== '') {
+            let newList = {
+                name: listName,
+                exercises: exercises,
+                id: Date.now(),
+            } 
+            listArray.push(newList)
+            toast.success('Treino salvo com sucesso!')
+            localStorage.setItem("listArray", JSON.stringify(listArray));
+            setListName('')
+        } else if (listName.trim('') === '') {
+            toast.warn('Digite um nome para o treino!')
         }
-
-        listArray.push(newList)
-        console.log(listArray)
-        toast.success('Treino salvo com sucesso!')
-        localStorage.setItem("listArray", JSON.stringify(listArray));
     }
-      
+
+    const [selectedList, setSelectedList] = useState('')
+
+    const handleSelectedList = (e) => {
+        let newSelectedList = e.target.value
+
+        setSelectedList(newSelectedList)
+        const filteredList = listArray.filter((list) => list.id === +newSelectedList)
+
+        setExercises(filteredList[0].exercises)
+        toast.success('Treino carregado!')
+    }
+
+    const deleteList = (e) => {
+        const updatedListArray = listArray.filter((list) => list.id !== selectedList)
+        console.log(selectedList)
+        console.log(updatedListArray)
+        setListArray(updatedListArray)
+
+        localStorage.setItem("listArray", JSON.stringify(updatedListArray))
+        toast.error('Treino deletado!')
+    }
+
   return (
     <div>
     {showEditForm ? (
         <div>
-            <form className="inputs-container" onSubmit={updateExercise}>
+            <form
+                className="inputs-container" 
+                onSubmit={updateExercise}>
                 <input className='exercise-input' 
                         type="text"
                         placeholder='Nome do exercício' 
+                        maxLength='40'
                         required
                         value={exercise}
                         onChange={(e) => setExercise(e.target.value)} />
                 <div className='sets-reps-container'>
-                    <input className='sets-input'
-                            type="number"
-                            placeholder='séries'
-                            min='1'
-                            required
-                            value={sets}
-                            onChange={(e) => setSets(e.target.value)} />
-                    <input className='reps-input' 
-                            type="number"
-                            placeholder='repetições'
-                            min='1'
-                            required
-                            value={reps}
-                            onChange={(e) => setReps(e.target.value)} />
-                    <input className='reps-input' 
-                            type="number"
-                            placeholder='carga'
-                            min='1'
-                            required
-                            value={weights}
-                            onChange={(e) => setWeights(e.target.value)} />
-                    <button className='update-btn' type="submit">Atualizar</button>
-                    <button className='cancel-btn' onClick={() => showEditForm(false)}>Cancelar</button>
+                        <input className='sets-input'
+                                type="number"
+                                placeholder='séries'
+                                min='1'
+                                max='99'
+                                required
+                                value={sets}
+                                onChange={(e) => setSets(e.target.value)} />
+                        <input className='reps-input' 
+                                type="number"
+                                placeholder='repetições'
+                                min='1'
+                                max='99'
+                                required
+                                value={reps}
+                                onChange={(e) => setReps(e.target.value)} />
+                            <input className='reps-input' 
+                                    type="number"
+                                    placeholder='carga'
+                                    min='1'
+                                    max='999'
+                                    required
+                                    value={weights}
+                                    onChange={(e) => setWeights(e.target.value)} />
+                                <button className='update-btn' type="submit">Atualizar</button>
+                                 <button className='cancel-btn' onClick={() => showEditForm(false)}>Cancelar</button>
                 </div>
             </form>
         </div>
     ) : (
         <div>
-            <form className="inputs-container" onSubmit={handleSubmit}>
+            <form 
+                className="inputs-container" 
+                onSubmit={handleSubmit}>
                 <input className='exercise-input' 
                         type="text" 
                         placeholder='Digite o nome do exercício' 
                         name='exercise'
+                        maxLength='40'
                         value={exercise}
                         onChange={(e) => setExercise(e.target.value)} />
                 <div className='sets-reps-container'>
-                    <input className='sets-input' 
-                            type='number' 
-                            placeholder='séries'
-                            min='1'
-                            name='sets'
-                            value={sets} 
-                            onChange={(e) => setSets(e.target.value)} />
-                    <input className='reps-input' 
-                            type='number' 
-                            placeholder='repetições'
-                            min='1'
-                            name='reps'
-                            value={reps} 
-                            onChange={(e) => setReps(e.target.value)} />
-                    <input className='reps-input' 
-                            type='number' 
-                            placeholder='carga'
-                            min='1'
-                            name='weights'
-                            value={weights} 
-                            onChange={(e) => setWeights(e.target.value)} />
-                    <button className='submit-btn' type="submit" ><MdAddBox /></button>
+                        <input className='sets-input' 
+                                type='number' 
+                                placeholder='séries'
+                                min='1'
+                                max='99'
+                                name='sets'
+                                value={sets} 
+                                onChange={(e) => setSets(e.target.value)} />
+                        <input className='reps-input' 
+                                type='number' 
+                                placeholder='repetições'
+                                min='1'
+                                max='99'
+                                value={reps} 
+                                onChange={(e) => setReps(e.target.value)} />
+                        <input className='reps-input' 
+                                type='number' 
+                                placeholder='carga'
+                                min='1'
+                                max='999'
+                                name='weights'
+                                value={weights} 
+                                onChange={(e) => setWeights(e.target.value)} />
+                        <button className='submit-btn' type="submit" ><MdAddBox /></button>
                 </div>
             </form>
         </div>)}
@@ -193,11 +233,11 @@ function Builder() {
                     <div className={showEditForm ? 'exercise selected' :
                                     ex.finished ? 'exercise finished' :
                                     'exercise'} >
-                        <input  style={{width: '22px'}} onChange={(e) => finishedToggle(e, ex.id)} checked={ex.finished} type='checkbox'></input>
+                        <input  className='input-checkbox' onChange={(e) => finishedToggle(e, ex.id)} checked={ex.finished} type='checkbox'></input>
                         <p className="exercise-name">{ex.exerciseName}</p>
                         <div className="stats">
                             <p style={ex.finished ? { backgroundColor: '#818f8950'} : {backgroundColor: '#84a78480'}} className='sets-number'>{ex.setsNum} x {ex.repsNum}</p>
-                            <p style={ex.finished ? { backgroundColor: '#818f8950'} : {backgroundColor: '#e7656580'}} className='weights-number'>{ex.weightsNum}kg</p>
+                            <p style={ex.finished ? { backgroundColor: '#818f8950'} : {backgroundColor: '#84a78480'}} className='weights-number'>{ex.weightsNum}kg</p>
                         </div>
                     </div>
                     <div className="controls">
@@ -208,13 +248,28 @@ function Builder() {
                 )))}
 
         <div className="save-container">
-            <input className='save-input' 
-                    type='text' 
-                    placeholder='Nome da Ficha'
-                    name='list'
-                    value={listName} 
-                    onChange={(e) => setListName(e.target.value)} />
-            <button className="save-btn" onClick={handleSave} type='submit'><MdAddBox /></button>
+            {exercises.length>0 && (
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <input className='save-input' 
+                            type='text' 
+                            placeholder='Nome do treino'
+                            name='list'
+                            maxLength='20'
+                            required
+                            value={listName} 
+                            onChange={(e) => setListName(e.target.value)} />
+                    <button className="save-btn" onClick={handleSave} type='submit'><MdAddBox /></button>
+                </div>)}
+
+        <div className="selector-container">
+            {listArray.length>0 && (
+                <select name="list" value={selectedList} onChange={(e) => handleSelectedList(e)} className="list-selector">
+                    {(listArray.map((list) => (
+                        <option value={list.id} id={list.id} className="option-name">{list.name}</option>
+                    )))}
+                </select>)}             
+            {listArray.length>0 ? <button className="delete-btn" onClick={deleteList}><MdDelete /></button> : ''}
+        </div>
         </div>
         </div>
     </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import TTD from '../../images/TTD.png'
 function Header() {
   return (
     <div className='header-container'>
-        <img src={TTD} alt="logo" style={{ height: '250px', width: '250px'}} />
+        <img src={TTD} alt="logo" className='header-logo' />
     </div>
   )
 }
